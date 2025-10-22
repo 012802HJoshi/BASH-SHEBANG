08703917fefb1fc098b50604724a066bc6f5b576
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-22 16:16:48 +0530

    one elden ring

--- elden_ring.sh
+++ elden_ring.sh
@@ -10,9 +10,15 @@ echo "Prepare to battle. Pick a number between 0-1. (0/1)"
 read yourmove
 
 if [[ $BEAST == $yourmove ]]; then
-    echo "Beast Vanquised!! Congratulations"
+    if [[ $USER == "ashishsaini" ]]; then
+        echo "Beast Vanquised!! Congratulations"
+    else 
+        echo "You Died"
+        exit 1
+    fi
 else
     echo "You Died"
+    exit 1
 fi
 
 sleep 2
@@ -22,4 +28,15 @@ sleep 2
 echo "Boss Battle, Get Ready..."
 echo "It's Margit. Pick a number between 0-9. (0/9)"
 
-read 
+read yourmove
+
+BEAST=$(($RANDOM % 10))
+
+if [[ $BEAST == $yourmove || $yourmove == coffee ]]; then
+    echo "Margit is Vanquised!! Congratulations"
+elif [[ $USER == "ashishsaini" ]]; then
+     echo "THIS USER ALWAYS WIN !!!"
+else 
+   echo "You Died"
+   exit 1
+fi
