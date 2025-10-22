ac53627eac1ea7625622807aab062dda9a116699
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-22 17:50:30 +0530

    SWITCH CASE and IF ELSE ELIF

--- elden_ring.sh
+++ elden_ring.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-# First Beast Battle 
+echo "Welcome to the Elden Ring Game. Please slect your Player: 
+1 - Samurai
+2 - Prisoner
+3 - Prophet"
+
+read Player
+
+case $Player in
+    1) 
+        type="Samurai"
+        hp=10
+        attack=20
+        ;;
+    2)
+        type="Prisoner"
+        hp=20
+        attack=4
+        ;;
+    3)
+        type="Prophet"
+        hp=30
+        attack=4
+        ;;
+esac
+
+echo "You have chosen the $type. Your HP is $hp and your attack is $attack."
 
 BEAST=$(($RANDOM % 2))
 
