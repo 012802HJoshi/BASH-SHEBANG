3c687b19b8d0da9b6db12baa4b5b0ce5dd79b6fc
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-23 15:54:47 +0530

    some more

--- basics2.sh
+++ basics2.sh
@@ -51,39 +51,3 @@ do
   echo "Hey, you've have $cups cups of coffee today."
 done
 
-# Site Tester
-
-for x in google.com bing.com facebook.com networkchuck.com;
-do
-    if ping -q -c 2 -W 1 $x > /dev/null; then
-        echo "$x is up"
-    else
-        echo "$x is down"
-    fi
-done
-
-
-# Weather Checker
-
-for x in $(cat cities.txt);
-do 
-  weather=$(curl -s http://wttr.in/$x?format=3)
-  echo "$weather"
-done
-
-# Site Up Checker
-
-echo "what do yo want to check ?"
-
-read target
-
-while true
-do 
-  if ping -q -c 2 -W 1 $target > /dev/null; then
-    echo "Hey, you're up !!"
-    break
-  else
-    echo "$target is currently down."
-  fi
-sleep 2
-done
