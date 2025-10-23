aeb0aaacc7f68aba75f9ec8aa46bd8420fe3344a
Harshit Joshi <012802HJoshi@users.noreply.github.com>
2025-10-23 12:36:27 +0530

    While Loop For Loop

--- basics2.sh
+++ basics2.sh
@@ -19,3 +19,46 @@ if [[ $coffee == "y" ]]; then
 else
     echo "Leave right now !!!!"
 fi
+
+# while
+x=1
+
+while [[ $x -le 100 ]]
+do
+  read -p "Pushup $x: Press enter to continue"
+  (( x ++ ))
+done
+echo "Congrats, you completed your pushups!! "
+
+# Until Loop
+
+until [[ $order == "coffee" ]]
+do
+    echo " Would you like coffee or tea ?"
+    read order
+done
+echo " Excellent choice, here is your coffee."
+
+# For Loop
+
+for cups in 1 2 3 4 5 6 7 8 9 10;
+do
+  echo "Hey, you've have $cups cups of coffee today."
+done
+
+for cups in {1..10};
+do
+  echo "Hey, you've have $cups cups of coffee today."
+done
+
+# Site Tester
+
+for x in google.com bing.com facebook.com networkchuck.com;
+do
+    if ping -q -c 2 -W 1 $x > /dev/null; then
+        echo "$x is up"
+    else
+        echo "$x is down"
+    fi
+done
+
